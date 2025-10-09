1dd4e1af063873fedf619ffae50815301ebadb74
Lim Nicolas <1387951+Nicnl@users.noreply.github.com>
2025-10-10 00:04:18 +0200

    Jakobs manual shortening progression log

--- lib/serial_tokenizer/walking_analysis_test.go
+++ lib/serial_tokenizer/walking_analysis_test.go
@@ -1,51 +1,48 @@
 package serial_tokenizer
 
 import (
+	"borderlands_4_serials/lib/b85"
 	"fmt"
 	"testing"
 )
 
 func TestByWalkingAnalysis(t *testing.T) {
 	serials := []string{
-		"0000101101011101001010100011001011000001010101011011101001010100000000000",
-		"0000101101011011001010100011001011000001010101011011101001010100000000000",
-		"0000101100011001001010100011001011000001010101011011101001010100000000000",
-		"0000101111011001001010100011001011000001010101011011101001010100000000000",
-		"0000101101111001001010100011001011000001010101011011101001010100000000000",
-		"0000101101010001001010100011001011000001010101011011101001010100000000000",
-		"0000101001011001001010100011001011000001010101011011101001010100000000000",
-		"0000101101011000001010100011001011000001010101011011101001010100000000000",
+		"@Ug!pHG38o5YT`HzQ)h-nP",
+		"@Ug!pHG38o5YZ7QZg)h-nP",
+		"@Ug!pHG38o5YOe&^9)h-nP",
+		"@Ug!pHG38o6@O)92A)h-nP",
+		"@Ug!pHG38o5YPb#KC)h-nP",
+		"@Ug!pHG38o5YMJlF2)h-nP",
+		"@Ug!pHG38o4tO)92A)h-nP",
+		"@Ug!pHG38o5Y4JxKV)h-nP",
 	}
 
 	serialInts := make(map[string][]int64)
 
-OUTER:
 	for _, s := range serials {
-		data := binToBytes(s)
+		data, _ := b85.Decode(s)
 		tok := NewTokenizer(data)
 
-		for i := 0; i < len(s); i++ {
+		for i := 0; i < len(data)*8; i++ {
 			tok.bs.SetPos(i)
 
-			token, err := tok.nextToken()
-			if err != nil {
-				break OUTER
-			}
+			//token, err := tok.nextToken()
+			//if err != nil {
+			//	continue
+			//}
 
-			if token == TOK_VARINT {
+			{
 				val, err := tok.readVarInt()
-				if err != nil {
-					t.Errorf("Error reading varint at pos %d in serial %s: %v", i, s, err)
-					continue OUTER
+				if err == nil {
+					serialInts[s] = append(serialInts[s], int64(val))
 				}
-				serialInts[s] = append(serialInts[s], int64(val))
-			} else if token == TOK_VARBIT {
+			}
+			{
 				val, err := tok.readVarBit()
-				if err != nil {
-					t.Errorf("Error reading varbit at pos %d in serial %s: %v", i, s, err)
-					continue OUTER
+				if err == nil {
+					serialInts[s] = append(serialInts[s], int64(val))
 				}
-				serialInts[s] = append(serialInts[s], int64(val))
 			}
 		}
 		fmt.Println("Serial:", s)
